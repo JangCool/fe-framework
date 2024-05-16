8d4de2538f4552de4b7fad55ce2d20eb3f6193e8
장진철 <zerocooldog@pionnet.co.kr>
2024-05-16 17:21:32 +0900

    fix : 초기 설정
    -
    
    업무 ID(생략가능) :
    
    ----------------------------------------
    다음 양식에 따라 commit 메세지를 작성해 주세요.
    작성 된후 해당 예제 내용은 제거해 주세요.
    
    <타입> : <제목>
    <본문 (생략 가능)>
    
    <그 외의 참고이슈 등등... (생략 가능)>
    
    Git Commit 메세지 작성 규칙 URL : https://app.clickup.com/9003193785/v/dc/8ca3ndt-19542/8ca3ndt-12242
    ----------------------------------------

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+
+    
+    /**
+     * 프로젝트에서 사용할 모듈 등록
+     */
+    modules: [
+        '@vueuse/nuxt',
+        '@pinia/nuxt'
+    ],
+
+    components:[
+        {
+            path: '~/components',
+
+            // components에 하위 폴더가 존재하면 하위 폴더명으로 컴포넌트명이 자동으로 생성 된다.
+            // components/base/Button.vue 라면 컴포넌트의 이름은 BaseButton이 된다.
+            // 추후에 중복 이름이 생길경우 찾아가기 힘들기 때문에 폴더명으로 이름이 생성되는 옵션인 pathPrefix를 false로 지정한다.
+            // https://nuxt.com/docs/guide/directory-structure/components#component-names
+            pathPrefix: false, 
+        },
+    ],
+
+    typescript: {
+        //nuxt 에서는 성능상의 이유로 타입스크립트를 검증하지 않는다.
+        //빌드시에 타임 유형을 체크 하려면 true로 변경 한다. 단. 빌드 속도가 조금 느리다.
+        //빌드 속도를 높이려면 false로 정의하여 타임 유형 체크를 하지 않는다.
+        //이 옵션을 끄려면 개발자가 로컬환경에서 개발할 때 typecheck명령어를 실행하여 커밋하기전에 수시로 확인하도록 룰을 정한다.
+        typeCheck: false
+    },
+
+    //pages 폴더에 컴포넌트를 생성하면 페이지 경로(URI)를 자동 생성한다.
+    pages: true
+
+})
